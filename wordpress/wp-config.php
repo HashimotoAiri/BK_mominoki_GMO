e3c2f2b9689fc8805eff72ff5c89bbc63791f34c
AiriHashimoto <airi@Mac-Airi.local>
2018-01-12 10:09:42 +0900

    GMOサーバーのbackup、以降完了分

---
+++ wordpress/wp-config.php
@@ -0,0 +1,58 @@
+<?php
+
+
+// ** MySQL settings ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'LAA0579494-qehg7m');
+
+/** MySQL database username */
+define('DB_USER', 'LAA0579494');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'w7eA4nQX');
+
+/** MySQL hostname */
+define('DB_HOST', 'mysql020.phy.lolipop.lan');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+define('AUTH_KEY',         ')_^Ot#-!9wyN)UzLp]Wc0:[&X$kHt|S+G/l( ow;)=;+k(io-R#`Q|y-E[wve(dK');
+define('SECURE_AUTH_KEY',  '!|>_WMiRFan[4;uD$Ryr9+k&wdG7+k=~9&1P~9:ey<}6b&JF^QPl=S]W/[uDh4@V');
+define('LOGGED_IN_KEY',    'vy8)4`hIrgTAK+?7+E$N*l{^$]+.hmlyMDOb6gp7!Z@_!6|TgkKIgAC-]$[yRYh&');
+define('NONCE_KEY',        '2}$.sz]$=Z>JJxd!|qZ-r-SpJ5I3sS&]I1i!~1/_--?I2|kbTK@}A<;%z-->H_z|');
+define('AUTH_SALT',        '@}>6TjE2C5X++$AXN.w9<{%J6u{Xvk$-wC_REiPJPX-pXdF:gu(PaSHH*tsY)#RU');
+define('SECURE_AUTH_SALT', '-}^PLUfUER+7 6k4pqGBp3I8(K(wIpgffJFSb:3/`cY(G226DRA9udV6<mW}NFC8');
+define('LOGGED_IN_SALT',   'R6L,k$>IH/?CfQU9I#;HA`]yu@oQxM,(GGem_?GHS,U{:[Mc</s_{g<;B+n#KQrp');
+define('NONCE_SALT',       's/b@h;8)m[sNj<&M`cADuq1Fe|XqE7Yq.JUM[@}I3F|Az+eZi/z~|avtO+|HN/s-');
+
+
+$table_prefix = 'wp_';
+
+
+define('FS_METHOD', 'direct');
+define('FS_CHMOD_DIR', 0775);
+define('FS_CHMOD_FILE', 0664);
+define('WP_AUTO_UPDATE_CORE', false);
+
+define('WP_CACHE', false);
+
+/** WordPress Custom Config. **/
+define('ZWP_BRAND_KEY', 'wpc');
+define('WP_DEBUG', False);
+define('ZWP_REGION_NAME', 'tyo1');
+
+
+
+
+/* That's all, stop editing! Happy blogging. */
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
---
+++ wordpress/wp-content/themes/fsv002wp-basic-c02/sidebar-footer.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * The sidebar containing the main widget area
+ *
+ * @package WordPress
+ * @subpackage FSV002WP BASIC
+ * @since FSV002WP BASIC 1.0
+ */
+?>
+
+<div id="footer-area-151016">
+	<div id="sub" class="footer-widget-area" role="complementary">
+		<div class="component-inner">
+
+			<div id="footer-area-151016-1" class="widget-area">
+<img src="http://mominoki-shihou.site/wp-content/uploads/2016/01/foot_logo.png" class="fo_logo" alt="司法書士法人もみのき事務所">
+
+<p class="hw_text_large"><img src="http://mominoki-shihou.site/wp-content/themes/fsv002wp-basic-c02/images/soudan_muryo.png" class="soudan_muryo"><a href="tel:0353474715">03-5347-4715</a></p><p class="hw_text">平日00:00～00:00  夜間・土日祝日対応</p>
+
+<p class="fo_address">〒167-0051</p>
+<p class="fo_address">東京都杉並区荻窪4丁目21-12</p>
+<p class="fo_address">ＪＲ・東京メトロ荻窪駅より徒歩3分</p>
+<a href="contactform.html"><img border="0" src="<?php bloginfo('template_directory'); ?>/images/foot_contact.png" class="fo_contact" alt="お問い合わせはこちらから"></a>
+			</div><!-- #footer-area-1 -->
+
+			<div id="footer-area-151016-2" class="widget-area">
+<h3>個人のお客様</h3>
+<ul>
+<li><a href="http://mominoki-shihou.site/kakusyuhiyou-meyasu/fudousan-touki-hiyou.html"><i class="fa fa-caret-square-o-right" style="font-size: 18px; color: #006666;"></i> 不動産名義変更・保存登記</a></li>
+<li><a href="http://mominoki-shihou.site/regist.html"><i class="fa fa-caret-square-o-right" style="font-size: 18px; color: #006666;"></i> 相続手続</a></li>
+<li><a href="http://mominoki-shihou.site/regist.html"><i class="fa fa-caret-square-o-right" style="font-size: 18px; color: #006666;"></i> 遺言手続き</a></li>
+<li><a href="http://mominoki-shihou.site/regist.html"><i class="fa fa-caret-square-o-right" style="font-size: 18px; color: #006666;"></i> 成年後見人</a></li>
+<li><a href="http://mominoki-shihou.site/saimuseiri.html"><i class="fa fa-caret-square-o-right" style="font-size: 18px; color: #006666;"></i> 債務整理</a></li>
+<li><a href="http://mominoki-shihou.site/regist/teitoken_massho.html"><i class="fa fa-caret-square-o-right" style="font-size: 18px; color: #006666;"></i> 抵当権の抹消（住宅ローンの完済）</a></li>
+</ul>
+			</div><!-- #footer-area-2 -->
+
+
+			<div id="footer-area-151016-3" class="widget-area">
+<h3>法人のお客様</h3>
+<ul>
+<li><a href="http://mominoki-shihou.site/kaisyaseturitu.html"><i class="fa fa-caret-square-o-right" style="font-size: 18px; color: #006666;"></i> 会社登記</a></li>
+<li><a href="http://mominoki-shihou.site/kakusyuhiyou-meyasu/syougyouhousyuu.html"><i class="fa fa-caret-square-o-right" style="font-size: 18px; color: #006666;"></i> 商業登記</a></li>
+<li><a href="http://mominoki-shihou.site/kakusyuhiyou-meyasu/fudousan-touki-hiyou.html"><i class="fa fa-caret-square-o-right" style="font-size: 18px; color: #006666;"></i> 不動産登記</a></li>
+<li><a href="http://mominoki-shihou.site/houmus.html"><i class="fa fa-caret-square-o-right" style="font-size: 18px; color: #006666;"></i> 契約書作成</a></li>
+</ul>
+			</div><!-- #footer-area-3 -->
+
+			<div id="footer-area-151016-4" class="widget-area">
+<h3>その他</h3>
+<ul>
+<li><a href="http://mominoki-shihou.site/access.html"><i class="fa fa-caret-square-o-right" style="font-size: 18px; color: #006666;"></i> アクセス</a></li>
+<li><a href="http://mominoki-shihou.site/contactform.html"><i class="fa fa-caret-square-o-right" style="font-size: 18px; color: #006666;"></i> お問い合わせ</a></li>
+<li><a href="http://mominoki-shihou.site/copyright.html"><i class="fa fa-caret-square-o-right" style="font-size: 18px; color: #006666;"></i> 著作権・免責事項</a></li>
+<li><a href="http://mominoki-shihou.site/privacy.html"><i class="fa fa-caret-square-o-right" style="font-size: 18px; color: #006666;"></i> 個人情報保護ポリシー</a></li>
+</ul>
+			</div><!-- #footer-area-4 -->
+
+
+
+		</div><!-- .component-inner -->
+	</div>
+</div>
+		<div class="clear"></div>
